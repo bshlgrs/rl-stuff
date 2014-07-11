28ef336d2505508b0d644653c7c23b24523f8119
Buck Shlegeris <bshlegeris@gmail.com>
2014-07-10 17:49:12 -0700

    i do not know how to haskell at all

--- n_armed_bandit.hs
+++ n_armed_bandit.hs
@@ -1,3 +1,18 @@
-data Lever = Lever { mean :: Float }
-  deriving (Show)
+import System.Random
+import Data.Array
 
+-- this shit is hard
+
+leverVariance = 1.0
+
+type BanditProblem = [Float]
+
+type ValueTable = Array Int (Float, Int)
+
+updateValueTable :: ValueTable -> Int -> Float -> ValueTable
+updateValueTable table pos newVal = let (oldAverage, numTrials) = table!pos in
+            table // (pos, ((oldAverage + newVal) / (numTrials + 1), numTrials + 1))
+
+chooseMove :: ValueTable -> Float -> [Float] -> IO Int
+chooseMove table epsilon randoms = do
+    ???
